6e06667c51c742dd4e3c128ed4a097f58ae8d758
Luis <luis.espinoza.nav@live.com>
2021-01-22 02:18:14 -0300

    Add JWT Token, and verify filter

--- src/main/java/com/jardin/api/filters/VerifyJwtToken.java
+++ src/main/java/com/jardin/api/filters/VerifyJwtToken.java
@@ -1,11 +1,12 @@
 package com.jardin.api.filters;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -14,7 +15,18 @@ import java.io.IOException;
 import java.util.Objects;
 
 @Component
-public class VerifyJwtToken implements Filter {
+public class VerifyJwtToken extends OncePerRequestFilter {
+/*
+    @Bean
+    public FilterRegistrationBean<VerifyJwtToken> loggingFilter(){
+        FilterRegistrationBean<VerifyJwtToken> registrationBean
+                = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new VerifyJwtToken());
+        registrationBean.addUrlPatterns("management/jardin-api/v1/garment/*");
+
+        return registrationBean;
+    }
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
 
@@ -23,7 +35,7 @@ public class VerifyJwtToken implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         String secretKey = "asegurandoJardinAPI2021LuisHECTOResPINOZAnaVaRRete9488888";
-
+        System.out.println("DEEEENTRO  PAPAPAPAPAPAP");
         HttpServletRequest req = (HttpServletRequest) request;
         String sessionToken = req.getHeader("sessionToken");
         HttpServletResponse res = (HttpServletResponse) response;
@@ -39,8 +51,12 @@ public class VerifyJwtToken implements Filter {
                 } else {
                     res.setHeader("isTokenValid", "false");
                 }
+            } catch (ExpiredJwtException expiredJwtException) {
+                System.out.println("Sesion vencida!!!");
+                System.out.println("token session expired");
+                res.setHeader("isTokenValid", "false");
             } catch (JwtException e) {
-                throw new RuntimeException(e);
+                System.out.println("Dentro de TOKEN NO VALIDO !!!!!!!!");
             }
         } else {
             res.setHeader("isTokenValid", "false");
@@ -51,5 +67,40 @@ public class VerifyJwtToken implements Filter {
     @Override
     public void destroy() {
 
+    }*/
+@Override
+protected boolean shouldNotFilter(HttpServletRequest request)  throws ServletException {
+    return new AntPathMatcher().match("/management/jardin-api/v1/loginToken", request.getServletPath());
+}
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws ServletException, IOException {
+
+        String secretKey = "asegurandoJardinAPI2021LuisHECTOResPINOZAnaVaRRete9488888";
+        String sessionToken = req.getHeader("sessionToken");
+        if (sessionToken != null) {
+            try {
+                Jws<Claims> JwtBody = Jwts.parserBuilder() //Si falla este parseo, va a JwtException, puede ir ahi si el token ha expirado por ejemplo.
+                        .setSigningKey(secretKey.getBytes())
+                        .build()
+                        .parseClaimsJws(sessionToken);
+                if (JwtBody.getBody().getSubject().equals("Jardin")) {
+                    res.setHeader("isTokenValid", "true");
+                } else {
+                    res.setHeader("isTokenValid", "false");
+                }
+            } catch (ExpiredJwtException expiredJwtException) {
+                System.out.println("Sesion vencida!!!");
+                System.out.println("token session expired");
+                res.setHeader("isTokenValid", "false");
+            } catch (JwtException e) {
+                System.out.println("Dentro de TOKEN NO VALIDO !!!!!!!!");
+            }
+        } else {
+            res.setHeader("isTokenValid", "false");
+        }
+        chain.doFilter(req, res);
     }
+
+
 }
