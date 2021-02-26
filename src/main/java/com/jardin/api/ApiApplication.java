8a5a588483aba736ecbd9b7121567bc79ec67c7e
Luis <luis.espinoza.nav@live.com>
2021-02-26 03:49:38 -0300

    deploy

--- src/main/java/com/jardin/api/ApiApplication.java
+++ src/main/java/com/jardin/api/ApiApplication.java
@@ -21,13 +21,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaAuditing
 @EnableJpaRepositories({ "com.jardin.api.repositories" })
 @EntityScan({ "com.jardin.api.model.entities", "com.jardin.api.services" })
-public class ApiApplication extends SpringBootServletInitializer   {
+public class ApiApplication   {
 
-
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-    return builder.sources(ApiApplication.class);
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(ApiApplication.class, args);
